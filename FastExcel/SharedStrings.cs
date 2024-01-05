fcdcf11b6128a1ac897d3bacd1e6b08aaf7f004a
berkaybyr <berkaybyr@icloud.com>
2024-01-05 16:14:03 +0300

    SharedStrings.cs fix; There was 2 dictionary one which forcing SharedStrings.xml to have unique strings which it does not have
    SharedStrings.xml does not have to contain unique strings this was causing exceptions when reading
    
    Instead now there is only 1 dictionary which is index and strings it can contain duplicate strings but index is unique

--- FastExcel/SharedStrings.cs
+++ FastExcel/SharedStrings.cs
@@ -1,172 +1,134 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.IO.Compression;
 using System.Linq;
+using System.Threading;
 using System.Xml;
 using System.Xml.Linq;
 
 namespace FastExcel
 {
+  /// <summary>
+  /// Read and update xl/sharedStrings.xml file
+  /// </summary>
+  public class SharedStrings
+  {
+    //A dictionary is a lot faster than a list
+    private Dictionary<int, string> StringDictionary { get; set; }
+
+    private bool SharedStringsExists { get; set; }
+    private ZipArchive ZipArchive { get; set; }
+
     /// <summary>
-    /// Read and update xl/sharedStrings.xml file
+    /// Is there any pending changes
     /// </summary>
-    public class SharedStrings
-    {
-        //A dictionary is a lot faster than a list
-        private Dictionary<string, int> StringDictionary { get; set; }
-        private Dictionary<int, string> StringArray { get; set; }
+    public bool PendingChanges { get; private set; }
 
-        private bool SharedStringsExists { get; set; }
-        private ZipArchive ZipArchive { get; set; }
+    /// <summary>
+    /// Is in read/write mode
+    /// </summary>
+    public bool ReadWriteMode { get; set; }
 
-        /// <summary>
-        /// Is there any pending changes
-        /// </summary>
-        public bool PendingChanges { get; private set; }
+    internal SharedStrings(ZipArchive archive) {
+      ZipArchive = archive;
+      SharedStringsExists = false;
 
-        /// <summary>
-        /// Is in read/write mode
-        /// </summary>
-        public bool ReadWriteMode { get; set; }
+      StringDictionary = new Dictionary<int, string>();
 
-        internal SharedStrings(ZipArchive archive)
-        {
-            ZipArchive = archive;
+      var sharedStringsXmlExists = ZipArchive.Entries.Any(entry => entry.FullName == "xl/sharedStrings.xml");
+      if (!sharedStringsXmlExists) {
+        SharedStringsExists = false;
+        return;
+      }
 
-            SharedStringsExists = true;
-
-            if (!ZipArchive.Entries.Where(entry => entry.FullName == "xl/sharedStrings.xml").Any())
-            {
-                StringDictionary = new Dictionary<string, int>();
-                SharedStringsExists = false;
-                return;
-            }
-
-            using (Stream stream = ZipArchive.GetEntry("xl/sharedStrings.xml").Open())
-            {
-                if (stream == null)
-                {
-                    StringDictionary = new Dictionary<string, int>();
-                    SharedStringsExists = false;
-                    return;
-                }
-
-                var document = XDocument.Load(stream);
-
-                if (document == null)
-                {
-                    StringDictionary = new Dictionary<string, int>();
-                    SharedStringsExists = false;
-                    return;
-                }
-
-                // int i = 0;
-                // StringDictionary = document.Descendants().Where(d => d.Name.LocalName == "t").Select(e => e.Value).Select(XmlConvert.DecodeName).to
-                //     .ToDictionary(k => k, v => i++);
-                int i = 0;
-                StringDictionary = new Dictionary<string, int>();
-                List<string> StringList = new List<string>();
-                StringList = document.Descendants().Where(d => d.Name.LocalName == "t").Select(e => XmlConvert.DecodeName(e.Value)).ToList();
-                foreach (string currentString in StringList)
-                {
-                    if (!StringDictionary.ContainsKey(currentString))
-                        StringDictionary.Add(currentString, i++);
-                }
-            }
+      using (var stream = ZipArchive.GetEntry("xl/sharedStrings.xml")?.Open()) {
+        if (stream == null) {
+          return;
         }
 
-        internal int AddString(string stringValue)
-        {
-            if (StringDictionary.ContainsKey(stringValue))
-            {
-                return StringDictionary[stringValue];
-            }
-            else
-            {
-                PendingChanges = true;
-                StringDictionary.Add(stringValue, StringDictionary.Count);
+        var document = XDocument.Load(stream) ?? throw new Exception("Failed to load sharedStrings.xml");
 
-                // Clear String Array used for retrieval
-                if (ReadWriteMode && StringArray != null)
-                {
-                    StringArray.Add(StringDictionary.Count - 1, stringValue);
-                }
-                else
-                {
-                    StringArray = null;
-                }
-
-                return StringDictionary.Count - 1;
-            }
+        SharedStringsExists = true;
+        var i = 0;
+        var stringList = document.Descendants().Where(d => d.Name.LocalName == "t").Select(e => XmlConvert.DecodeName(e.Value)).ToList();
+        /*
+         * NOTE: sharedStrings.xml can contain duplicate strings, but the index is always unique
+         * so we don't care about duplicated and just add them to the dictionary by index
+         */
+        foreach (var currentString in stringList) {
+          StringDictionary.Add(i++, currentString);
         }
-
-        internal void Write()
-        {
-            // Only update if changes were made
-            if (!PendingChanges)
-            {
-                return;
-            }
-
-            StreamWriter streamWriter = null;
-            try
-            {
-                if (SharedStringsExists)
-                {
-                    streamWriter = new StreamWriter(ZipArchive.GetEntry("xl/sharedStrings.xml").Open());
-                }
-                else
-                {
-                    streamWriter = new StreamWriter(ZipArchive.CreateEntry("xl/sharedStrings.xml").Open());
-                }
-
-                // TODO instead of saving the headers then writing them back get position where the headers finish then write from there
-
-                /* Note: the count attribute value is wrong, it is the number of times strings are used thoughout the workbook it is different to the unique count 
-                 *       but because this library is about speed and Excel does not seem to care I am not going to fix it because I would need to read the whole workbook
-                 */
-
-                streamWriter.Write(string.Format("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>" +
-                            "<sst uniqueCount=\"{0}\" count=\"{0}\" xmlns=\"http://schemas.openxmlformats.org/spreadsheetml/2006/main\">", StringDictionary.Count));
-
-                // Add Rows
-                foreach (var stringValue in StringDictionary)
-                {
-                    streamWriter.Write(string.Format("<si><t>{0}</t></si>", XmlConvert.EncodeName(stringValue.Key)));
-                }
-
-                //Add Footers
-                streamWriter.Write("</sst>");
-                streamWriter.Flush();
-            }
-            finally
-            {
-                streamWriter.Dispose();
-                PendingChanges = false;
-            }
-        }
-
-        internal string GetString(string position)
-        {
-            if (int.TryParse(position, out int pos))
-            {
-                return GetString(pos + 1);
-            }
-            else
-            {
-                // TODO: should I throw an error? this is a corrupted excel document
-                return string.Empty;
-            }
-        }
-
-        internal string GetString(int position)
-        {
-            if (StringArray == null)
-            {
-                StringArray = StringDictionary.ToDictionary(kv => kv.Value, kv => kv.Key);
-            }
-
-            return StringArray[position - 1];
-        }
+      }
     }
+
+    internal int AddString(string stringValue) {
+      var nextIndex = StringDictionary.Count;
+      StringDictionary.Add(nextIndex, stringValue);
+      if (!ReadWriteMode) {
+        return -1;
+      }
+
+      PendingChanges = true;
+      return nextIndex;
+    }
+
+    internal void Write() {
+      // Only update if changes were made
+      if (!PendingChanges) {
+        return;
+      }
+
+      StreamWriter streamWriter = null;
+      try {
+        streamWriter = SharedStringsExists
+                         ? new StreamWriter(ZipArchive.GetEntry("xl/sharedStrings.xml")?.Open() ?? throw new Exception("Failed to open xl/sharedStrings.xml"))
+                         //This exception will never throw because we check if the file exists before we get the stream
+                         : new StreamWriter(ZipArchive.CreateEntry("xl/sharedStrings.xml").Open());
+
+        // TODO instead of saving the headers then writing them back get position where the headers finish then write from there
+
+        /* Note: the count attribute value is wrong, it is the number of times strings are used thoughout the workbook it is different to the unique count
+         *       but because this library is about speed and Excel does not seem to care I am not going to fix it because I would need to read the whole workbook
+         */
+
+        var textToWrite = string.Format("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>" +
+                                        "<sst uniqueCount=\"{0}\" count=\"{0}\" xmlns=\"http://schemas.openxmlformats.org/spreadsheetml/2006/main\">",
+                                        StringDictionary.Count);
+        streamWriter.Write(textToWrite);
+
+        // Add Rows
+        foreach (var stringValue in StringDictionary) {
+          streamWriter.Write($"<si><t>{XmlConvert.EncodeName(stringValue.Value)}</t></si>");
+        }
+
+        //Add Footers
+        streamWriter.Write("</sst>");
+        streamWriter.Flush();
+      }
+      finally {
+        streamWriter?.Dispose();
+        PendingChanges = false;
+      }
+    }
+
+    internal string GetString(string position) {
+      if (int.TryParse(position, out int pos)) {
+        return GetString(pos + 1);
+      }
+
+      // TODO: should I throw an error? this is a corrupted excel document
+      throw new Exception("Corrupted excel document position: " + position);
+      return string.Empty;
+    }
+
+    internal string GetString(int position) {
+      var valueExist = StringDictionary.TryGetValue(position - 1, out var value);
+      return !valueExist
+               //TODO SHOULD THROW ? 
+               // throw new Exception("String does not exist in shared strings position: " + position);
+               ? string.Empty
+               : value;
+    }
+  }
 }
